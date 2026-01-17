556f056cecc21087cd07f4d0cbca6d654f325bb2
khadijaabirat <khadijaabirat@gmail.com>
2026-01-17 22:05:34 +0100

    controlers

--- App/Controllers/AuthController.php
+++ App/Controllers/AuthController.php
@@ -1,10 +1,43 @@
 <?php
 namespace App\Controllers;
 
-class AuthController extends BaseController {
-    public function loginForm(): void {
-        $this->render('login.html.twig', [
-            'title' => 'Login - FacileAchat'
-        ]);
+use App\Models\User;
+use App\Models\UserRepository;
+
+class AuthController {
+    private $userRepo;
+    private $twig;
+
+public function __construct($twig, UserRepository $userRepo) {
+        $this->twig = $twig;
+        $this->userRepo = $userRepo;
+    }
+
+    public function showRegister() {
+        echo $this->twig->render('auth/register.html.twig');
+    }
+
+    public function handleRegister() {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $name = $_POST['name'] ?? '';
+            $email = $_POST['email'] ?? '';
+            $password = $_POST['password'] ?? '';
+
+            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+            $user = new User([
+                'name' => $name,
+                'email' => $email,
+                'password_hash' => $hashedPassword,
+                'total_points' => 0
+            ]);
+
+            if ($this->userRepo->save($user)) {
+                header('Location: /Loyalty_Points_System/public/login');
+                exit;
+            } else {
+                echo "erreur";
+            }
+        }
     }
 }
--- App/Models/UserRopo.php
+++ App/Models/UserRopo.php
@@ -1,5 +1,7 @@
 <?php
 namespace App\Models;
+
+use App\Models\User;
 use App\Core\Database;
 use PDO;
 
